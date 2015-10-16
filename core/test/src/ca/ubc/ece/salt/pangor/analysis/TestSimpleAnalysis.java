1100e9c1e55e4ee18ac54e84df227712f3d731ff
Quinn Hanam <qhanam@ece.ubc.ca>
2015-10-15 17:44:23 -0700

    Added a test class for CommitAnalysis.

---
+++ core/test/src/ca/ubc/ece/salt/pangor/analysis/TestSimpleAnalysis.java
@@ -0,0 +1,19 @@
+package ca.ubc.ece.salt.pangor.analysis;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Tests {@code CommitAnalysis} and {@code SourceCodeFileAnalysis} with
+ * {@code SimpleSourceCodeFileAnalysis}.
+ */
+public class TestSimpleAnalysis {
+
+	@Test
+	public void test() {
+		/* TODO */
+		fail("Not yet implemented");
+	}
+
+}
