ad1ac87d541d5df7caaf7094b97f553bcafd437e
Quinn Hanam <qhanam@ece.ubc.ca>
2016-01-14 20:40:47 -0800

    Added datalog queries to core test cases.

--- core/test/src/ca/ubc/ece/salt/pangor/test/analysis/TestBatchAnalysis.java
+++ core/test/src/ca/ubc/ece/salt/pangor/test/analysis/TestBatchAnalysis.java
@@ -1,12 +1,23 @@
 package ca.ubc.ece.salt.pangor.test.analysis;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.deri.iris.api.basics.ILiteral;
+import org.deri.iris.api.basics.IPredicate;
+import org.deri.iris.api.basics.IQuery;
+import org.deri.iris.api.basics.IRule;
+import org.deri.iris.api.basics.ITuple;
+import org.deri.iris.api.factory.IBasicFactory;
+import org.deri.iris.api.factory.ITermFactory;
+import org.deri.iris.api.terms.ITerm;
+import org.deri.iris.factory.Factory;
 import org.junit.Assert;
 import org.junit.Test;
 
 import ca.ubc.ece.salt.pangor.analysis.CommitAnalysis;
-import ca.ubc.ece.salt.pangor.analysis.simple.SimpleAlert;
+import ca.ubc.ece.salt.pangor.analysis.DomainAnalysis;
 import ca.ubc.ece.salt.pangor.analysis.simple.SimpleCFGFactory;
 import ca.ubc.ece.salt.pangor.analysis.simple.SimpleDataSet;
 import ca.ubc.ece.salt.pangor.analysis.simple.SimpleDstFileAnalysis;
@@ -18,8 +29,29 @@ public class TestBatchAnalysis {
 	@Test
 	public void test() throws IOException, Exception {
 
+		/* Create the Datalog queries. */
+		IBasicFactory basicFactory = Factory.BASIC;
+		ITermFactory termFactory = Factory.TERM;
+
+		ITerm termX = termFactory.createString("X"); // X
+		ITerm termY = termFactory.createString("Y"); // Y
+		ITuple tuple = basicFactory.createTuple(termX, termY); // X,Y
+
+		IPredicate sourcePredicate = basicFactory.createPredicate("SourceRoot", 2); // SourceRoot(,)
+		ILiteral source = basicFactory.createLiteral(true, sourcePredicate, tuple); // SourceRoot(X,Y)
+		IQuery sourceQuery = basicFactory.createQuery(source); // ?- SourceRoot(X,Y).
+
+		IPredicate destinationPredicate = basicFactory.createPredicate("DestinationRoot", 2); // DestinationRoot(,)
+		ILiteral destination = basicFactory.createLiteral(true, destinationPredicate, tuple); // DestinationRoot(X,Y)
+		IQuery destinationQuery = basicFactory.createQuery(destination); // ?- DestinationRoot(X,Y).
+
+		List<IRule> rules = new LinkedList<IRule>();
+		List<IQuery> queries = new LinkedList<IQuery>();
+		queries.add(sourceQuery);
+		queries.add(destinationQuery);
+
 		/* Set up the data set (stores alerts aka feature vectors). */
-		SimpleDataSet dataSet = new SimpleDataSet();
+		SimpleDataSet dataSet = new SimpleDataSet(rules, queries);
 
 		/* We will use a simple file analysis for this test. */
 		SimpleSrcFileAnalysis srcAnalysis = new SimpleSrcFileAnalysis();
@@ -28,12 +60,16 @@ public class TestBatchAnalysis {
 		/* The SimpleCFGFactory doesn't do anything, so it is generic to any file type. */
 		SimpleCFGFactory cfgFactory = new SimpleCFGFactory();
 
-		/* Set up the commit analysis (analyzes one commit). */
-		CommitAnalysis<SimpleAlert, SimpleDataSet,
-					   SimpleSrcFileAnalysis, SimpleDstFileAnalysis> commitAnalysis;
-		commitAnalysis = new CommitAnalysis<SimpleAlert, SimpleDataSet,
-					   SimpleSrcFileAnalysis, SimpleDstFileAnalysis>(
-							   dataSet, srcAnalysis, dstAnalysis, cfgFactory, false);
+		/* Set up the domain analysis. */
+		DomainAnalysis domainAnalysis;
+		domainAnalysis = new DomainAnalysis(srcAnalysis, dstAnalysis,
+											cfgFactory, false);
+		List<DomainAnalysis> domainAnalyses = new LinkedList<DomainAnalysis>();
+		domainAnalyses.add(domainAnalysis);
+
+		/* Set up the commit analysis. */
+		CommitAnalysis commitAnalysis;
+		commitAnalysis = new CommitAnalysis(dataSet, domainAnalyses);
 
 		/* Set up the project analysis (analyzes one project). */
 		GitProjectAnalysis projectAnalysis = GitProjectAnalysis.fromURI(
--- core/test/src/ca/ubc/ece/salt/pangor/test/analysis/TestSimpleAnalysis.java
+++ core/test/src/ca/ubc/ece/salt/pangor/test/analysis/TestSimpleAnalysis.java
@@ -6,6 +6,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.deri.iris.api.basics.ILiteral;
+import org.deri.iris.api.basics.IPredicate;
+import org.deri.iris.api.basics.IQuery;
+import org.deri.iris.api.basics.IRule;
+import org.deri.iris.api.basics.ITuple;
+import org.deri.iris.api.factory.IBasicFactory;
+import org.deri.iris.api.factory.ITermFactory;
+import org.deri.iris.api.terms.ITerm;
+import org.deri.iris.factory.Factory;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -41,8 +50,29 @@ public class TestSimpleAnalysis {
 
 		commit.addSourceCodeFileChange(sourceFileChange);
 
+		/* Create the Datalog queries. */
+		IBasicFactory basicFactory = Factory.BASIC;
+		ITermFactory termFactory = Factory.TERM;
+
+		ITerm termX = termFactory.createString("X"); // X
+		ITerm termY = termFactory.createString("Y"); // Y
+		ITuple tuple = basicFactory.createTuple(termX, termY); // X,Y
+
+		IPredicate sourcePredicate = basicFactory.createPredicate("SourceRoot", 2); // SourceRoot(,)
+		ILiteral source = basicFactory.createLiteral(true, sourcePredicate, tuple); // SourceRoot(X,Y)
+		IQuery sourceQuery = basicFactory.createQuery(source); // ?- SourceRoot(X,Y).
+
+		IPredicate destinationPredicate = basicFactory.createPredicate("DestinationRoot", 2); // DestinationRoot(,)
+		ILiteral destination = basicFactory.createLiteral(true, destinationPredicate, tuple); // DestinationRoot(X,Y)
+		IQuery destinationQuery = basicFactory.createQuery(destination); // ?- DestinationRoot(X,Y).
+
+		List<IRule> rules = new LinkedList<IRule>();
+		List<IQuery> queries = new LinkedList<IQuery>();
+		queries.add(sourceQuery);
+		queries.add(destinationQuery);
+
 		/* Set up the data set (stores alerts aka feature vectors). */
-		SimpleDataSet dataSet = new SimpleDataSet(/*List<IRule>*/, /*List<IQuery>*/);
+		SimpleDataSet dataSet = new SimpleDataSet(rules, queries);
 
 		/* We will use a simple file analysis for this test. */
 		SimpleSrcFileAnalysis srcAnalysis = new SimpleSrcFileAnalysis();
