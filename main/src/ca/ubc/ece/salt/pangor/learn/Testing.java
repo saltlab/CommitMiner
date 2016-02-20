2353ba00cb70cd51cf9ad73e7b333132b95984c5
Quinn Hanam <qhanam@ece.ubc.ca>
2016-02-19 20:54:24 -0800

    Added lambda expression for plotting charts.

--- main/src/ca/ubc/ece/salt/pangor/learn/Testing.java
+++ main/src/ca/ubc/ece/salt/pangor/learn/Testing.java
@@ -5,10 +5,13 @@ public class Testing {
 
 	public static void main(String[] args) {
 
-		EvaluationResult[][] results = new EvaluationResult[1][3];
+		EvaluationResult[][] results = new EvaluationResult[2][3];
 		results[0][0] = new EvaluationResult(null, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1);
 		results[0][1] = new EvaluationResult(null, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2);
 		results[0][2] = new EvaluationResult(null, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3);
+		results[1][0] = new EvaluationResult(null, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05, 0.1);
+		results[1][1] = new EvaluationResult(null, 0.2, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2);
+		results[1][2] = new EvaluationResult(null, 0.3, 0.3, 0.3, 0.3, 0.3, 0.2, 0.3);
 		printRChart(results);
 
 	}
@@ -30,20 +33,7 @@ public class Testing {
 		script += "par(mfrow = c(3,1))\n";
 		script += "par(oma = c(4,1,1,1))\n";
 		script += "\n";
-
-		script += "chartName <- \"Inspected vs Epsilon\"\n";
-		script += "\n";
-		script += String.format("xData <- c(%s)\n", getEpsilon(results[0]));
-		script += String.format("yData <- c(%s)\n", getInspected(results[0]));
-		script += "plot(xData,yData,main=chartName,xlab=\"epsilon\",ylab=\"Inspected\",ylim=c(0,0.3),xlim=c(0.1,5.9),yaxp=c(0,1,10),xaxp=c(0.1,5.9,29),yaxs=\"i\",xaxs=\"i\",type=\"l\")";
-
-		for(int i = 0; i < results.length; i++) {
-			EvaluationResult[] dataSetResult = results[i];
-			script += "\n";
-			script += String.format("yData <- c(%s)\n", getInspected(dataSetResult));
-			script += "lines(xData,yData,type=\"l\",lwd=1,lty=1,col=\"green\",pch=0)\n";
-		}
-
+		script += printChart(results, Testing::getEpsilon, Testing::getInspected);
 		script += "\n";
 		script += "par(fig=c(0,1,0,1),oma=c(0,0,0,0),mar=c(0,0,0,0),new=TRUE)\n";
 		script += "plot(0,0,type=\"n\", bty=\"n\",xaxt=\"n\",yaxt=\"n\")\n";
@@ -52,6 +42,29 @@ public class Testing {
 		script += "dev.off()";
 
 		System.out.println(script);
+
+		printChart(results, Testing::getEpsilon, Testing::getInspected);
+	}
+
+	public static String printChart(EvaluationResult[][] results, Data ydata, Data xdata) {
+		String script = "";
+		script += "chartName <- \"Inspected vs Epsilon\"\n";
+		script += "\n";
+		script += String.format("xData <- c(%s)\n", xdata.getData(results[0]));
+		script += String.format("yData <- c(%s)\n", ydata.getData(results[0]));
+		script += "plot(xData,yData,main=chartName,xlab=\"epsilon\",ylab=\"Inspected\",ylim=c(0,0.3),xlim=c(0.1,5.9),yaxp=c(0,1,10),xaxp=c(0.1,5.9,29),yaxs=\"i\",xaxs=\"i\",type=\"l\")\n";
+
+		for(int i = 0; i < results.length; i++) {
+			EvaluationResult[] dataSetResult = results[i];
+			script += String.format("yData <- c(%s)\n", getInspected(dataSetResult));
+			script += "lines(xData,yData,type=\"l\",lwd=1,lty=1,col=\"green\",pch=0)\n";
+		}
+
+		return script;
+	}
+
+	public interface Data {
+		String getData(EvaluationResult[] dataSetResult);
 	}
 
 	/**
