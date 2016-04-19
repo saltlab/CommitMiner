afde33241d75ebba04bbedaf5229f180ef57b890
Quinn Hanam <qhanam@ece.ubc.ca>
2016-04-18 18:35:17 -0700

    documentation for IAbstractState

--- core/src/ca/ubc/ece/salt/pangor/analysis/flow/IAbstractState.java
+++ core/src/ca/ubc/ece/salt/pangor/analysis/flow/IAbstractState.java
@@ -3,6 +3,11 @@ package ca.ubc.ece.salt.pangor.analysis.flow;
 import ca.ubc.ece.salt.pangor.cfg.CFGEdge;
 import ca.ubc.ece.salt.pangor.cfg.CFGNode;
 
+/**
+ * Stores the state of the abstract interpretation at a given point of the
+ * program. This could be a lattice element, or it could store many lattice
+ * elements (e.g., one for each variable in a dataflow analysis).
+ */
 public interface IAbstractState {
 
 	/**
